f1f576ff1e69059e68808252c6b00da7674ee985
tushita <mercuriodevelop@gmail.com>
2018-02-06 19:13:40 -0700

    rocking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { SuccessComponent } from './components/success/success.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: '404', component: NotfoundComponent },
+  { path: 'emailsuccess', component:SuccessComponent },
   { path: '**', redirectTo: '/404' }
 ];
 
